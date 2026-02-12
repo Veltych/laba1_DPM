ba14f76abae8accbefe5bb8ef61ad6710ea03c2c
Veltych <sprrosraolpomml@gmail.com>
2026-02-12 09:43:28 +0700

    Update ProgramExample.cs

--- OOP_Fundamentals_Library/OOP_Fundamentals_Library/ProgramExample.cs
+++ OOP_Fundamentals_Library/OOP_Fundamentals_Library/ProgramExample.cs
@@ -4,44 +4,46 @@
     {
         ///// ДАННЫЙ КОД НЕ ЗАПУСТИТСЯ, ЭТО ПРИМЕР ИСПОЛЬЗОВАНИЯ КЛАССОВ ИЗ БИБЛИОТЕКИ OOP_Fundamentals_Library
         ///// СКОПИРУЙТЕ ЕГО В МЕТОД MAIN ОСНОВОГО ПРОЕКТА ДЛЯ ПРОВЕРКИ РАБОТЫ
-        //static void Main(string[] args)
-        //{
-        //    var customer = new Customer
-        //    {
-        //        Name = "John",
-        //        Age = 30
-        //    };
-
-        //    var employee = new Employee
-        //    {
-        //        Name = "Alice",
-        //        Age = 25,
-        //        Salary = 50000,
-        //        Position = "Developer"
-        //    };
-
-        //    var manager = new Manager
-        //    {
-        //        Name = "Bob",
-        //        Age = 40,
-        //        Salary = 80000,
-        //        Department = "IT"
-        //    };
-
-        //    //manager.Team.Add(employee);
-
-        //    employee.Salary = 55000;
-
-        //    customer.PrintInfo();
-        //    employee.PrintInfo();
-        //    manager.PrintInfo();
-
-        //    var payroll = new PayrollSystem();
-        //    payroll.ProcessSalary(employee);
-        //    payroll.ProcessSalary(manager);
-
-        //    ReportService.GenerateEmployeeReport(employee);
-        //    ReportService.GenerateManagerReport(manager);
-        //}
+        static void Main(string[] args)
+        {
+            var customer = new Customer
+            {
+                Name = "John",
+                Age = 30
+            };
+            
+            var employee = new Employee
+            {
+                Name = "Alice",
+                Age = 25,
+                Position = "Developer"
+            };
+            employee.IncreaseSalary(50000);
+            
+            var manager = new Manager
+            {
+                Name = "Bob",
+                Age = 40,
+                Department = "IT"
+            };
+            manager.IncreaseSalary(80000);
+            
+            manager.Team.Add(employee);
+            
+            customer.PrintInfo();
+            employee.PrintInfo();
+            manager.PrintInfo();
+            
+            employee.ProcessPayroll();
+            manager.ProcessPayroll();
+            
+            employee.ReportService();
+            manager.ReportService();
+            
+            employee.IncreaseSalary(5000);
+            Console.WriteLine($"Encrease salary {employee.Name}: {employee.Salary}");
+            
+            manager.AssignTaskToEmployee(employee, "Implement new feature");
+       }
     }
 }
